4dd6293ee31edd4a7d8fbdfb10cbb51342f6bae1
sid-gambhir <siddhant.gambhir@retainiq.io>
2023-05-08 13:03:31 +0530

    home page changes

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -1,24 +1,82 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from 'react'
-import { Popover, Transition } from '@headlessui/react'
-import { MenuIcon, XIcon } from '@heroicons/react/outline'
-
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { Disclosure, Menu } from '@headlessui/react'
+import { BellIcon} from '@heroicons/react/outline'
 
 const buttonList = [
-    { name: 'Search by keyword', initials: 'GA', href: '/browse-keywords', members: 16, bgColor: 'bg-pink-600' },
-    { name: 'Search by BLAST', initials: 'CD', href: '/browse-blast', members: 12, bgColor: 'bg-purple-600' },
-    { name: 'Submit', initials: 'T', href: '/submit', members: 16, bgColor: 'bg-yellow-500' },
-    { name: 'Contact us', initials: 'RC', href: '/contact-us', members: 8, bgColor: 'bg-green-500' },
-  ]
-  
-  function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
-  }
+  {
+    name: "Search by keyword",
+    initials: "GA",
+    href: "/browse-keywords",
+    members: 16,
+    bgColor: "bg-pink-600",
+  },
+  {
+    name: "Search by BLAST",
+    initials: "CD",
+    href: "/browse-blast",
+    members: 12,
+    bgColor: "bg-purple-600",
+  },
+  {
+    name: "Submit",
+    initials: "T",
+    href: "/submit",
+    members: 16,
+    bgColor: "bg-yellow-500",
+  },
+  {
+    name: "Contact us",
+    initials: "RC",
+    href: "/contact-us",
+    members: 8,
+    bgColor: "bg-green-500",
+  },
+];
+const navigation = [
+  {
+    name: "Search by keyword",
+    initials: "GA",
+    href: "/browse-keywords",
+    members: 16,
+    bgColor: "bg-pink-600",
+  },
+  {
+    name: "Search by BLAST",
+    initials: "CD",
+    href: "/browse-blast",
+    members: 12,
+    bgColor: "bg-purple-600",
+  },
+  {
+    name: "Submit",
+    initials: "T",
+    href: "/submit",
+    members: 16,
+    bgColor: "bg-yellow-500",
+  },
+  {
+    name: "Contact us",
+    initials: "RC",
+    href: "/contact-us",
+    members: 8,
+    bgColor: "bg-green-500",
+  },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 export default function LandingPage() {
   return (
     <div className="relative  overflow-hidden">
-      <div className="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full" aria-hidden="true">
+      <div
+        className="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full"
+        aria-hidden="true"
+      >
         <div className="relative h-full max-w-7xl mx-auto">
           <svg
             className="absolute right-full transform translate-y-1/4 translate-x-1/4 lg:translate-x-1/2"
@@ -36,10 +94,21 @@ export default function LandingPage() {
                 height={20}
                 patternUnits="userSpaceOnUse"
               >
-                <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                <rect
+                  x={0}
+                  y={0}
+                  width={4}
+                  height={4}
+                  className="text-gray-200"
+                  fill="currentColor"
+                />
               </pattern>
             </defs>
-            <rect width={404} height={784} fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
+            <rect
+              width={404}
+              height={784}
+              fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
+            />
           </svg>
           <svg
             className="absolute left-full transform -translate-y-3/4 -translate-x-1/4 md:-translate-y-1/2 lg:-translate-x-1/2"
@@ -57,18 +126,32 @@ export default function LandingPage() {
                 height={20}
                 patternUnits="userSpaceOnUse"
               >
-                <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                <rect
+                  x={0}
+                  y={0}
+                  width={4}
+                  height={4}
+                  className="text-gray-200"
+                  fill="currentColor"
+                />
               </pattern>
             </defs>
-            <rect width={404} height={784} fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)" />
+            <rect
+              width={404}
+              height={784}
+              fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)"
+            />
           </svg>
         </div>
       </div>
 
       <div className="relative pt-6 pb-16 sm:pb-24">
         <Popover>
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 ">
-            <nav className="relative flex items-center justify-between sm:h-10 md:justify-center " aria-label="Global">
+          {/* <div className="max-w-7xl mx-auto px-4 sm:px-6 ">
+            <nav
+              className="relative flex items-center justify-between sm:h-10 md:justify-center "
+              aria-label="Global"
+            >
               <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
                 <div className="flex items-center justify-between w-full md:w-auto">
                   <a href="#">
@@ -79,6 +162,7 @@ export default function LandingPage() {
                       alt=""
                     />
                   </a>
+                  <p className="font-medium text-indigo-600 hover:text-gray-900 text-3xl pl-4">MecDB</p>
                   <div className="-mr-2 flex items-center md:hidden">
                     <Popover.Button className="bg-gray-50 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                       <span className="sr-only">Open main menu</span>
@@ -87,25 +171,98 @@ export default function LandingPage() {
                   </div>
                 </div>
               </div>
-              {/* <div className="hidden md:flex md:space-x-10">
+              <div className="hidden md:flex md:space-x-10">
                 {navigation.map((item) => (
-                  <a key={item.name} href={item.href} className="font-medium text-gray-500 hover:text-gray-900">
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="font-medium text-gray-500 hover:text-gray-900 text-2xl"
+                  >
                     {item.name}
                   </a>
                 ))}
-              </div> */}
+              </div>
               <div className="hidden md:absolute md:flex md:items-center md:justify-end md:inset-y-0 md:right-0">
-                {/* <span className="inline-flex rounded-md shadow">
-                  <a
-                    href="#"
-                    className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50"
-                  >
-                    Log in
-                  </a>
-                </span> */}
+                
               </div>
             </nav>
+          </div> */}
+
+          {/* alternate nav bar */}
+          <Disclosure as="nav" className="bg-gray-800">
+      {({ open }) => (
+        <>
+          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-16">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="items-center">
+                  {/* <img
+                    className="block lg:hidden h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                    alt="Workflow"
+                  /> */}
+                  {/* <img
+                    className="hidden lg:block h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                    alt="Workflow"
+                  /> */}
+                  <p className="font-medium text-white  text-4xl pl-4">MecDB </p>
+                  {" "}
+                  
+                </div>
+                <div className="hidden sm:block ml-14 pb-0">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'bg-gray-900 text-white' : 'text-gray-200 hover:bg-gray-700 hover:text-white',
+                          'px-3 py-2 rounded-md text-lg font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              
+            </div>
           </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block px-3 py-2 rounded-md text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </a>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
 
           <Transition
             as={Fragment}
@@ -121,7 +278,7 @@ export default function LandingPage() {
               className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
             >
               <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                <div className="px-5 pt-4 flex items-center justify-between">
+                <div className="px-5 pt-4 flex items-end justify-between">
                   <div>
                     <img
                       className="h-8 w-auto"
@@ -129,7 +286,6 @@ export default function LandingPage() {
                       alt=""
                     />
                   </div>
-                  
                 </div>
                 {/* <div className="px-2 pt-2 pb-3">
                   {navigation.map((item) => (
@@ -156,12 +312,28 @@ export default function LandingPage() {
         <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
           <div className="text-center">
             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-              <span className="block xl:inline">Welcome to</span>{' '}
-              <span className="block text-indigo-600 xl:inline underline">DREAM database</span>
+              <span className="block xl:inline">Welcome to</span>{" "}
+              <span className="block text-indigo-600 xl:inline ">
+              MecDB
+              </span>
+              <br/>
+
             </h1>
-            <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat fugiat aliqua.
+            <p className="font-medium text-gray-700 text-2xl">Monocots experimentally characterized gene Database </p>
+
+            <p className="mt-3 max-w-md mx-auto text-base text-gray-600 sm:text-lg md:mt-5 md:text-xl md:max-w-5xl">
+              Welcome to MecDB, a database of manually curated data of
+              experimentally characterized genes in monocots. Each entry
+              contains information for gene name, host plant, plant species in
+              which it is characterized, accession number, method used for
+              characterization, pathway category, and phenotype. All the said
+              information can be downloaded by the user in excel format. The
+              data is obtained from publicly available databases like PDB and
+              NCBI. 
+              <br/>
+              PlecDB currently contains <b>122</b> experimentally characterized
+              gene from five monocot plant species Finger millet, Foxtail
+              millet, Sorghum bicolor, Pearl millet, Hordeum vulgare.
             </p>
             <div className="mt-5  mx-auto sm:flex sm:justify-center md:mt-8">
               {/* <div className="rounded-md shadow">
@@ -173,25 +345,32 @@ export default function LandingPage() {
                 </a>
               </div> */}
               {/* alternate buttons */}
-              <ul role="list" className="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {buttonList.map((singleButton) => (
-          <li key={singleButton.name} className="col-span-1 flex shadow-sm rounded-md">
-            
-            <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-gray-800  rounded-r-md truncate">
-              <div className="flex-1 px-4 py-2 text-sm truncate">
-                <a href={singleButton.href} className="text-gray-200  font-medium text-lg">
-                  {singleButton.name}
-                </a>
-              </div>
-              
-            </div>
-          </li>
-        ))}
-      </ul>
+              {/* <ul
+                role="list"
+                className="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4"
+              >
+                {buttonList.map((singleButton) => (
+                  <li
+                    key={singleButton.name}
+                    className="col-span-1 flex shadow-sm rounded-md"
+                  >
+                    <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-gray-800  rounded-r-md truncate">
+                      <div className="flex-1 px-4 py-2 text-sm truncate">
+                        <a
+                          href={singleButton.href}
+                          className="text-gray-200  font-medium text-lg"
+                        >
+                          {singleButton.name}
+                        </a>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul> */}
             </div>
           </div>
         </main>
       </div>
     </div>
-  )
+  );
 }
